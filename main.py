bc84f52d41f4185cec8cd882e8c8d0d42a6d4917
Sohum-Tripathi <68846631+Mcprince12@users.noreply.github.com>
2021-04-19 08:30:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ path = "/Users/sohumtripathi/Downloads"
 now = time.time()
 
 for filename in os.listdir(path):
-    filestamp = os.stat(os.path.join(path, filename)).st_mtime
-    filecompare = now - 7 * 86400
-    if filestamp < filecompare:
-        print(filename)
+    # if os.stat(os.path.join(path, filename)).st_mtime < now - 7 * 86400:
+    if os.path.getmtime(os.path.join(path, filename)) < now - 7 * 86400:
+        if os.path.isfile(os.path.join(path, filename)):
+            print(filename)
+            os.remove(os.path.join(path, filename))
